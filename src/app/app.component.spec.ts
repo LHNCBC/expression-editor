bf821392afc2b7132ea95ea838262040cdde30d3
Mazilu, Mihai (NIH/NLM/LHC) [C] <mihai.mazilu@nih.gov>
2022-02-08 14:49:26 -0500

    Fix failing test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Demo Application');
+    expect(compiled.querySelector('h1').textContent).toContain('Rule Editor Demo');
   });
 });
