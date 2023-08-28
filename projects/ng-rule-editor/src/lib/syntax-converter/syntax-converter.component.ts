45eaa7a00d8e197d18b4efb53ec67f4de33962b2
Woody Amornkul <amornkulw2@lhc-lx-lformdev.nlm.nih.gov>
2023-08-28 13:07:07 -0400

    Added help dialog and accessibility for the Easy Path Expression

--- projects/ng-rule-editor/src/lib/syntax-converter/syntax-converter.component.ts
+++ projects/ng-rule-editor/src/lib/syntax-converter/syntax-converter.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, Output, EventEmitter, OnChanges } from '@angular/core';
 import { EasyPathExpressionsPipe } from '../easy-path-expressions.pipe';
 import { SimpleStyle } from '../rule-editor.service';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
 
 @Component({
   selector: 'lhc-syntax-converter',
@@ -14,11 +15,13 @@ export class SyntaxConverterComponent implements OnChanges {
   @Input() lhcStyle: SimpleStyle = {};
   @Output() simpleChange = new EventEmitter<string>();
   @Output() expressionChange = new EventEmitter<string>();
- 
+  
   fhirPathExpression: string;
   jsToFhirPathPipe = new EasyPathExpressionsPipe();
 
-  constructor() {}
+  constructor(private liveAnnouncer: LiveAnnouncer) {}
+  
+  showHelp = false;
 
   ngOnChanges(): void {
     this.onExpressionChange(this.simple);
@@ -31,4 +34,13 @@ export class SyntaxConverterComponent implements OnChanges {
     this.simpleChange.emit(simple);
     this.expressionChange.emit(fhirPath);
   }
+
+  openHelp(): void {
+    this.liveAnnouncer.announce("Open Easy Path Expression help modal dialog. Easy Path Expression is a syntax parser that converts basic mathematical expressions into FHIRPath notation.  This dialog is divided into three sections: Available variables, Usable Operators, and Usable Functions.  Use tab to navigate between each sections."); 
+    this.showHelp = true;
+  }
+
+  closeHelp(): void {
+    this.showHelp = false;
+  } 
 }
