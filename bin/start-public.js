647098a66361e821d1860652c70286fc8303dea5
Woody Amornkul <amornkulw2@lhc-lx-lformdev01.nlm.nih.gov>
2023-09-05 17:10:01 -0400

    Added source map

--- bin/start-public.js
+++ bin/start-public.js
@@ -5,7 +5,11 @@ const ngArgs = ['serve', '--host', hostName];
 if (process.argv[2] && process.argv[2] === '--dist') {
   ngArgs.push('--configuration');
   ngArgs.push('production');
+} else {
+  ngArgs.push('--configuration');
+  ngArgs.push('development');
 }
+
 spawn('ng', ngArgs, {
   stdio: 'inherit'
 });
