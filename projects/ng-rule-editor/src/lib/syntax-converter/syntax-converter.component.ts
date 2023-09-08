7707d1a0cbed9d127309a94fd85e0056c90c499a
Woody Amornkul <amornkulw2@lhc-lx-lformdev.nlm.nih.gov>
2023-09-08 11:05:00 -0400

    Added FHIRPath Expression help dialog and modified EasyPath Expression help dialog

--- projects/ng-rule-editor/src/lib/syntax-converter/syntax-converter.component.ts
+++ projects/ng-rule-editor/src/lib/syntax-converter/syntax-converter.component.ts
@@ -21,8 +21,6 @@ export class SyntaxConverterComponent implements OnChanges {
 
   constructor(private liveAnnouncer: LiveAnnouncer) {}
   
-  showHelp = false;
-
   ngOnChanges(): void {
     this.onExpressionChange(this.simple);
   }
@@ -34,13 +32,4 @@ export class SyntaxConverterComponent implements OnChanges {
     this.simpleChange.emit(simple);
     this.expressionChange.emit(fhirPath);
   }
-
-  openHelp(): void {
-    this.liveAnnouncer.announce("Open Easy Path Expression help dialog. Easy Path Expression is a syntax parser that converts basic mathematical expressions into FHIRPath notation.  This dialog is divided into three sections: Available variables, Usable Operators, and Usable Functions.  Use tab to navigate between each sections.");
-    this.showHelp = true;
-  }
-
-  closeHelp(): void {
-    this.showHelp = false;
-  } 
 }
