63a38982c88cfc24ba7e266f58f0a306f9395937
Woody Amornkul <amornkulw2@lhc-lx-lformdev01vm.nlm.nih.gov>
2023-11-13 21:15:32 -0500

    Updated configuraiton for sourceMap

--- bin/start-public.js
+++ bin/start-public.js
@@ -5,9 +5,6 @@ const ngArgs = ['serve', '--host', hostName];
 if (process.argv[2] && process.argv[2] === '--dist') {
   ngArgs.push('--configuration');
   ngArgs.push('production');
-} else {
-  ngArgs.push('--configuration');
-  ngArgs.push('development');
 }
 
 spawn('ng', ngArgs, {
