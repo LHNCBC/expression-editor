a42262ca63814a9284cf3d248ede86dff00a3743
Woody Amornkul <amornkulw2@lhc-lx-gldimg.nlm.nih.gov>
2024-04-01 18:32:38 -0400

    Added a screen reader error message for variables used in the output expression.

--- projects/ng-rule-editor/src/lib/validation.ts
+++ projects/ng-rule-editor/src/lib/validation.ts
@@ -5,6 +5,7 @@ export const FHIR_QUERY_OBSERVATION_REQUIRED = "Code for FHIR query is required.
 export const TIME_INTERVAL_REQUIRED = "Time interval is required.";
 
 export const INVALID_EXPRESSION = "Invalid expression.";
+export const INVALID_EXPRESSION_OUTPUT = "The output expression is no longer valid.";
 
 export const VARIABLE_NAME_REQUIRED = "Variable name is required.";
 export const VARIABLE_NAME_EXISTS_IN_ITEM = "Variable name is already in use on this item.";
