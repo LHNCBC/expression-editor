5ee70910a86f77a096242256c5ea90fb7618f6c8
Woody Amornkul <amornkulw2@lhc-lx-gldimg.nlm.nih.gov>
2024-06-25 11:37:05 -0400

    Updated function description

--- src/assets/js/common-utils.js
+++ src/assets/js/common-utils.js
@@ -1,3 +1,13 @@
+
+/**
+ * Set the display object to show/hide various sections of the Rule Editor
+ * based on the hiddenSectionStr.
+ * @param hiddenSectionStr - a comma-separated list of section names to be
+ * hidden. Possible values are:
+ * 
+ *  "titleSection", "uneditableVariablesSection", "itemVariablesSection", and
+ *  "outputExpressionSection"
+ */
 function createDisplayOption(hiddenSectionStr) {
   const displaySections = ["titleSection", "uneditableVariablesSection",
                            "itemVariablesSection", "outputExpressionSection"];
